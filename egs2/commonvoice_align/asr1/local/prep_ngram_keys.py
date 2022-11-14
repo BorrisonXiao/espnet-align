e00f61d9cb659ec21d511b0b4ef6febdff0b1cff
Cihan Xiao <cxiao7@b06.clsp.jhu.edu>
2022-11-14 14:40:58 -0500

    Implemented k2 decoding for hklegco data with biased graphs

--- egs2/commonvoice_align/asr1/local/prep_ngram_keys.py
+++ egs2/commonvoice_align/asr1/local/prep_ngram_keys.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from pathlib import Path
 from utils import scpid2mid, mkdir_if_not_exist
+from tqdm import tqdm
 
 
 def generate_scp(input_dir, output, mscript_dir, mlevel_keys):
@@ -13,11 +14,27 @@ def generate_scp(input_dir, output, mscript_dir, mlevel_keys):
     alignment.
     """
     recorded_mids = {}
-    with open(output, "w") as f:
-        with open(mlevel_keys, "w") as f2:
-            for scpid in os.listdir(input_dir):
+    if output:
+        with open(output, "w") as f:
+            with open(mlevel_keys, "w") as f2:
+                for scpid in os.listdir(input_dir):
+                    lm_train_fp = os.path.join(input_dir, scpid, "lm_train.txt")
+                    print(scpid, lm_train_fp, file=f)
+
+                    mid = scpid2mid(scpid)
+                    mid_dir = os.path.join(mscript_dir, mid)
+                    mkdir_if_not_exist(mid_dir)
+                    mscp = os.path.join(mid_dir, "lm_train.txt")
+                    recorded_mids[mid] = mscp
+                    with open(lm_train_fp, "r") as fp:
+                        with open(mscp, "a") as ofh:
+                            for line in fp:
+                                print(line.strip(), file=ofh)
+    else:
+        # Allow skipping the segment level ngram training as it is error-prone and time consuming
+        with open(mlevel_keys, "w") as f:
+            for scpid in tqdm(os.listdir(input_dir)):
                 lm_train_fp = os.path.join(input_dir, scpid, "lm_train.txt")
-                print(scpid, lm_train_fp, file=f)
 
                 mid = scpid2mid(scpid)
                 mid_dir = os.path.join(mscript_dir, mid)
@@ -30,7 +47,7 @@ def generate_scp(input_dir, output, mscript_dir, mlevel_keys):
                             print(line.strip(), file=ofh)
 
     with open(mlevel_keys, "w") as f:
-        for mid, fp in recorded_mids.items():
+        for mid, fp in tqdm(recorded_mids.items()):
             print(mid, fp, file=f)
 
 
@@ -39,7 +56,7 @@ def main():
         description='Prepare the keys for biased ngram training')
     parser.add_argument('--input_dir', type=Path, required=True,
                         help='Input: The directory containing the script files')
-    parser.add_argument('--slevel_keys', type=Path, required=True,
+    parser.add_argument('--slevel_keys', type=Path, default=None,
                         help='The generated segment level slevel_keys.scp file.')
     parser.add_argument('--mscript_dir', type=Path, required=True,
                         help='The directory to store the meeting level ngram training text for primary decoding.')
